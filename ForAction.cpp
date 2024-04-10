8db8f3d8077930521031edba98c0b83f73077167
Werl32 <165184998+Werl32@users.noreply.github.com>
2024-04-10 18:27:57 +0300

    Add files via upload

---
+++ ForAction.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 3;
+    int b = 15;
+    int sum = 0;
+    for (int i = 0; i < b; i++)
+    {
+        sum += i;
+        a++;
+    }
+    cout << sum;
+}
+
