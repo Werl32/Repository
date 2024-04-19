a8e68a498a0457770823a3a6be23e63b46a77c2b
Werl32 <165184998+Werl32@users.noreply.github.com>
2024-04-19 19:36:20 +0300

    Increase_Vector
    Увеличивает вектор на n с помощью функции

---
+++ App_Increase.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int Increase (vector <int> v, int n)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        v[i] += n;
+    }  
+    for (int i = 0; i < v.size(); i++)
+    {
+        return v[i];
+    }
+}
